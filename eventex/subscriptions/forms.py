b9d2fb3d1f7d1c50169b39164619273414923c86
luiz <luiz-olimpio@hotmail.com>
2016-01-20 20:35:13 -0300

    w

--- eventex/subscriptions/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-class SubscriptionForm(forms.Form):
-    name = forms.CharField()
-    cpf = forms.CharField()
-    email = forms.EmailField()
-    phone = forms.CharField()
-
-
-
