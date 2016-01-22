5c3dca87f6d824ac4a0810325d05c8e28cdff299
luiz <luiz-olimpio@hotmail.com>
2016-01-22 16:21:49 -0300

    instala django extensions

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ git clone git@github.com:luizolimpio/eventex.git wttd
 cd wttd
 python -m venv .wttd
 source .wttd\Scripts\Activate.bat
-pip install -r requirements.txt
+pip install -r requirements-dev.txt
 cp contrib/env-sample .env
 python manage.py test
 
--- eventex/settings.py
+++ eventex/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'eventex.core',
     'eventex.subscriptions',
 ]
@@ -119,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # email configuration
 
 EMAIL_BACKEND = config('EMAIL_BACKEND')
